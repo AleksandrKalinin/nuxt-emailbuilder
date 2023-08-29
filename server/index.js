ef9c7ff49a17aa54bef39157b5d2377d1a48da23
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2023-08-29 12:04:50 +0200

    fix: Remove redundant code

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }));
 app.use(bodyParser.json({ limit: "50mb" }));
 
+const link =
+  "https://images.unsplash.com/photo-1682687221248-3116ba6ab483?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80";
+
 app.post("/create", (req, res) => {
   const DOWNLOAD_DIR = path.join(
     process.env.HOME || process.env.USERPROFILE,
@@ -66,7 +69,7 @@ app.post("/image", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  sendMail(req.body.email, req.body.template);
+  sendMail(req.body.email, req.body.template, req.body.filepath);
 });
 
 const transporter = nodemailer.createTransport({
@@ -79,13 +82,20 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-async function sendMail(email, template) {
+async function sendMail(email, template, filepath) {
   const info = await transporter.sendMail({
     from: "1995kalininaleksandr@gmail.com",
     to: email,
     subject: "E-mail template",
     text: "Your template",
-    html: template,
+    attachments: [
+      {
+        filename: "template.html",
+        path: filepath,
+        filePath: filepath,
+        contentType: "text/html; charset=UTF-8",
+      },
+    ],
   });
 }
 
